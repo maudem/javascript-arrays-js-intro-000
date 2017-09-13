bd29b5dfbbca10c46a8f913948da46ec71124dea
maude sos <mualth@gmail.com>
2017-09-13 23:55:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var addElementToBeginningOfArray = (array, element) =>{
 }
 
 var destructivelyAddElementToBeginningOfArray =(array, element) =>{
-  element = array.unshift(element)
+  array.unshift(element)
   return array
 }
 
