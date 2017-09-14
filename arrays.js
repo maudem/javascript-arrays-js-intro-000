83de3d5f786b6beb7aa44253cfb8b0539a07a8c4
maude sos <mualth@gmail.com>
2017-09-14 00:05:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,8 +15,8 @@ var addElementToEndOfArray = (array, element) => {
   return array
 }
 
-var destructivelyRemoveElementFromEndOfArray = (array, element) =>{
-  array.pop(element)
+var destructivelyAddElementToEndOfArray = (array, element) => {
+ array.push(element)
   return array
 }
 
@@ -25,11 +25,12 @@ var removeElementFromBeginningOfArray = (array, element) =>{
   return array
 }
 
-var destructivelyAddElementToEndOfArray = (array, element) => {
- array.push(element)
+var destructivelyRemoveElementFromEndOfArray = (array, element) =>{
+  array.pop(element)
   return array
 }
 
 var accessElementInArray = (array, index )=> {
     return array[index]
 }
+
